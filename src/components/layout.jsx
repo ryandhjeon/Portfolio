5d7ef9361f24f75f23417bfe72d1d6d5f35ec800
Ryan Jeon <ryandhjeon@gmail.com>
2021-07-31 19:52:04 -0400

    Edit font

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -5,11 +5,16 @@ import PropTypes from 'prop-types';
 import { Navbar } from './navbar';
 import Footer from './footer';
 import { useSiteMetadata } from '../utils/useSiteMetadata';
+import { Helmet } from 'react-helmet'
+import { withPrefix } from 'gatsby'
 
 const Layout = ({ children }) => {
   const { title } = useSiteMetadata();
   return (
     <div sx={{ variant: 'layout' }}>
+      <Helmet>
+        <link href={withPrefix('https://use.typekit.net/cfv1yds.css')} rel="stylesheet" type="text/css" />
+      </Helmet>
       <Navbar siteTitle={title} />
       <main sx={{ variant: 'layout.main' }}>
         {children}
