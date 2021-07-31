5d13d37a74c9003fdeb6dae606d6b1e39f02f5ab
Ryan Jeon <ryandhjeon@gmail.com>
2021-07-31 19:59:43 -0400

    Add fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,10 +45,10 @@ module.exports = {
     {
       resolve: "gatsby-plugin-web-font-loader",
       options: {
-        typekit: {
-          id: process.env.TYPEKIT_ID,
-        },
-      },
+        google: {
+          families: ['Raleway']
+        }
+      }
     },
     // {
     //   resolve: `gatsby-plugin-typography`,
--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -5,16 +5,12 @@ import PropTypes from 'prop-types';
 import { Navbar } from './navbar';
 import Footer from './footer';
 import { useSiteMetadata } from '../utils/useSiteMetadata';
-import { Helmet } from 'react-helmet'
-import { withPrefix } from 'gatsby'
+import "../styles/index.css"
 
 const Layout = ({ children }) => {
   const { title } = useSiteMetadata();
   return (
     <div sx={{ variant: 'layout' }}>
-      <Helmet>
-        <link href={withPrefix('https://use.typekit.net/cfv1yds.css')} rel="stylesheet" type="text/css" />
-      </Helmet>
       <Navbar siteTitle={title} />
       <main sx={{ variant: 'layout.main' }}>
         {children}
